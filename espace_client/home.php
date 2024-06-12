6eeb2d005941095fab01b00322f73efd985baf0d
MalronClem <malronclement@mail.com>
2024-06-12 14:35:45 +0200

    début

---
+++ espace_client/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+    <main>
+        <div class="container" id="nv_projet">
+            <div class="title" >
+                <h1>NOUVEAU PROJET</h1>
+            </div>
+        </div>
+        <div class="container" id="suivis_projet">
+        <div class="title">
+                <h1>SUIVIS PROJET</h1>
+            </div>
+        </div>
+        <div class="container" id="moderation_devis">
+        <div class="title">
+                <h1>MODERATION DEVIS</h1>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
