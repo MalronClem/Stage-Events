3de8b8d09d57e831318f9cf4f403b5a4ce5aa2f2
MalronClem <malronclement@mail.com>
2024-07-25 19:11:04 +0200

    mise a jour

---
+++ footer_pages/mention_leg.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mention légales</title>
+    <link rel="stylesheet" href="footer_pages.css">
+</head>
+<body>
+    <nav>
+        <ul>
+                        <li data-content="prestations">
+                            <svg fill="#000000" width="800px" height="800px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <g>
+                                <path d="M1,19H5V15H1Zm7,0h4V15H8Zm7,0h4V15H15ZM1,12H5V8H1Zm7,0h4V8H8Zm7,0h4V8H15ZM1,5H5V1H1ZM8,5h4V1H8Zm7-4V5h4V1Z"/>
+                                </g>
+                            </svg>
+                            <p>Vos prestations</p>
+                        </li>
+                        <li data-content="messages">
+                            <svg class="svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.76953 4.58009C8.57706 3.74781 9.54639 3.08958 10.6178 2.64588C11.6892 2.20219 12.84 1.98233 13.9995 2.00001C18.4195 2.00001 21.9995 5.10005 21.9995 8.92005C21.9792 9.98209 21.7021 11.0234 21.1919 11.9551C20.6817 12.8867 19.9535 13.681 19.0696 14.27V16.75" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16 13.64C16 10.29 12.87 7.58008 9 7.58008C5.13 7.58008 2 10.29 2 13.64C2.01941 14.5684 2.26227 15.4785 2.70789 16.2931C3.1535 17.1077 3.78881 17.803 4.56006 18.3201V20.49C4.55903 20.7858 4.64489 21.0755 4.80701 21.3229C4.96912 21.5703 5.20032 21.7647 5.47192 21.8818C5.74353 21.999 6.04362 22.0338 6.33484 21.9819C6.62606 21.9301 6.89553 21.7938 7.10999 21.5901L9.10999 19.6901C12.94 19.6201 16 16.94 16 13.64Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <p>Messages</p>
+                        </li>
+                        <li data-content="accueil">
+                            <p>LOGO</p>
+                        </li>
+                        <li data-content="aide">
+                            <svg class="svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12 19.5C16.1421 19.5 19.5 16.1421 19.5 12C19.5 7.85786 16.1421 4.5 12 4.5C7.85786 4.5 4.5 7.85786 4.5 12C4.5 16.1421 7.85786 19.5 12 19.5ZM12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12.75 15V16.5H11.25V15H12.75ZM10.5 10.4318C10.5 9.66263 11.1497 9 12 9C12.8503 9 13.5 9.66263 13.5 10.4318C13.5 10.739 13.3151 11.1031 12.9076 11.5159C12.5126 11.9161 12.0104 12.2593 11.5928 12.5292L11.25 12.7509V14.25H12.75V13.5623C13.1312 13.303 13.5828 12.9671 13.9752 12.5696C14.4818 12.0564 15 11.3296 15 10.4318C15 8.79103 13.6349 7.5 12 7.5C10.3651 7.5 9 8.79103 9 10.4318H10.5Z" fill="#080341"/>
+                            </svg>
+                            <p>Aide</p>
+                        </li>
+                        <li data-content="deconnexion">
+                            <svg class="svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 3V12M18.3611 5.64001C19.6195 6.8988 20.4764 8.50246 20.8234 10.2482C21.1704 11.994 20.992 13.8034 20.3107 15.4478C19.6295 17.0921 18.4759 18.4976 16.9959 19.4864C15.5159 20.4752 13.776 21.0029 11.9961 21.0029C10.2162 21.0029 8.47625 20.4752 6.99627 19.4864C5.51629 18.4976 4.36274 17.0921 3.68146 15.4478C3.00019 13.8034 2.82179 11.994 3.16882 10.2482C3.51584 8.50246 4.37272 6.8988 5.6311 5.64001" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <p>Déconnexion</p>
+                        </li>
+                    
+        </ul>
+    </nav>
+    <main>
+        <div class="conf">
+            <div class="data">
+                <h1>Mention légales</h1>
+                <!--à adapter aux réelles mentions légales-->
+                <ul>
+                    <li>
+                        <h2>Introduction</h2>
+                        <p>[Nom de l'Organisation] s’engage à protéger la confidentialité de vos données personnelles. Cette politique de confidentialité décrit les types de données personnelles que nous collectons, comment nous les utilisons et les mesures que nous prenons pour garantir leur protection. En utilisant notre site, vous acceptez les pratiques décrites dans cette politique.</p>
+                    </li>
+                    <li>
+                        <h2>1- Types de Données Collectées</h2>
+                        <p> • Données Personnelles Identifiables : Lorsque vous vous inscrivez sur notre site, nous collectons des informations telles que votre nom, adresse e-mail, numéro de téléphone et adresse postale. <br>
+                            • Données de Navigation : Nous collectons également des informations sur votre utilisation du site, telles que votre adresse IP, le type de navigateur que vous utilisez, les pages visitées et les temps de visite. <br>
+                            • Données de Transaction : Informations relatives aux réservations et paiements effectués sur le site.<br>
+                            • Cookies et Technologies Similaires : Pour améliorer votre expérience utilisateur, nous utilisons des cookies et d’autres technologies similaires pour collecter des données de navigation.
+                        </p>
+                    </li>
+                    <li>
+                        <h2>2- Utilisation des Données</h2>
+                        <p> • Gestion de Compte : Les données collectées sont utilisées pour gérer votre compte, traiter vos réservations et communiquer avec vous concernant nos services. <br>
+                        • Amélioration du Service : Nous utilisons les informations collectées pour améliorer notre site et nos services, personnaliser votre expérience utilisateur et analyser les tendances du marché.<br>
+                        • Confort et Personnalisation : Certaines données collectées permettent de vous offrir une expérience plus confortable et personnalisée en comprenant mieux vos goûts et souhaits.<br>
+                        • Protection des Utilisateurs : Nous collectons également des données pour vous protéger en cas de besoin éventuel, comme lors d’une réclamation ou d’un litige.<br>
+                        • Marketing : Avec votre consentement, nous pouvons utiliser vos informations pour vous envoyer des offres promotionnelles, des newsletters et des informations sur les produits et services susceptibles de vous intéresser.<br>
+                        • Conformité Légale : Nous utilisons vos données pour nous conformer aux obligations légales et réglementaires.</p>
+                    </li>
+                    <li>
+                        <h2>3- Partage des Données</h2>
+                        <p> • Partenaires : Nous pouvons partager vos informations avec des partenaires de confiance qui nous aident à fournir nos services, tels que les prestataires de services de mariage, les plateformes de paiement et les hébergeurs de site.<br>
+                        • Obligations Légales : Nous pouvons divulguer vos données personnelles si la loi l’exige ou pour protéger nos droits légaux. <br>
+                        • Protection de Nos Intérêts : Pour protéger les droits, la propriété ou la sécurité de [Nom de l'Organisation], de nos utilisateurs ou d’autres parties, nous pouvons partager vos informations en cas de litige ou de réclamation.<br>
+                        • Avec Votre Consentement : Avec votre consentement explicite, nous pouvons partager vos données avec d’autres tiers.</p>
+                    </li>
+                    <li>
+                        <h2>4- Sécurité des Données</h2>
+                        <p> • Mesures de Sécurité : Nous mettons en œuvre diverses mesures de sécurité pour protéger vos informations personnelles contre les accès non autorisés, la divulgation, la modification ou la destruction. Ces mesures incluent l’utilisation de technologies de cryptage, de pare-feu et de contrôles d’accès. <br>
+                        • Responsabilité Utilisateur : Il est de votre responsabilité de garder confidentielles vos informations de connexion et de ne les partager avec personne.<br>
+                        • Notification en Cas de Violation : En cas de violation de la sécurité des données, [Nom de l'Organisation] s’engage à notifier les autorités compétentes et les utilisateurs concernés dans les meilleurs délais conformément à la réglementation en vigueur.</p>
+                    </li>
+                    <li>
+                        <h2>5- Droits des Utilisateurs</h2>
+                        <p> •Droit d’Accès : Vous avez le droit d’accéder à vos données personnelles que nous détenons.
+                        • Droit de Rectification : Vous pouvez demander la correction de vos données personnelles si elles sont inexactes ou incomplètes.
+                        • Droit à l’Effacement : Vous pouvez demander la suppression de vos données personnelles dans certaines circonstances.
+                        • Droit à la Limitation du Traitement : Vous pouvez demander que nous limitions le traitement de vos données personnelles dans certaines situations.
+                        • Droit à la Portabilité des Données : Vous avez le droit de recevoir vos données personnelles dans un format structuré, couramment utilisé et lisible par machine.<br>
+                        • Droit d’Opposition : Vous pouvez vous opposer au traitement de vos données personnelles dans certaines circonstances, y compris le traitement à des fins de marketing direct.</p>
+                    </li>
+                    <li>
+                        <h2>6- Cookies et Technologies Similaires</h2>
+                        <p> •Utilisation des Cookies : Le site [Nom de l'Organisation] utilise des cookies pour faciliter la navigation et pour personnaliser les informations proposées aux utilisateurs. Les cookies sont des fichiers textes placés sur votre ordinateur, pour aider le site à analyser l’utilisation du site par ses utilisateurs.<br>
+                        • Paramétrage des Cookies : Vous pouvez configurer votre navigateur pour refuser l’installation des cookies. Cependant, le refus des cookies peut entraîner l’impossibilité d’accéder à certaines fonctionnalités du site. En continuant à utiliser le site sans modifier vos paramètres, vous acceptez l’utilisation des cookies.</p>
+                    </li>
+                    <li>
+                        <h2>7- Conservation des Données
+                        </h2>
+                        <p>• Durée de Conservation : Nous conservons vos données personnelles aussi longtemps que nécessaire pour remplir les finalités pour lesquelles elles ont été collectées, y compris pour satisfaire aux exigences légales, comptables ou de rapport.<br>
+                        • Critères de Conservation : Les critères utilisés pour déterminer la période de conservation incluent la nature des données, la sensibilité des données, les risques potentiels de préjudice découlant de l’utilisation ou de la divulgation non autorisée de vos données, et les exigences légales applicables.</p>
+                    </li>
+                    <li>
+                        <h2>8- Modifications de la Politique de Confidentialité</h2>
+                        <p>• Mises à Jour : [Nom de l'Organisation] se réserve le droit de modifier cette politique de confidentialité à tout moment. Les modifications seront publiées sur cette page et entreront en vigueur dès leur publication. Nous vous encourageons à consulter régulièrement cette page pour prendre connaissance des mises à jour éventuelles.</p>
+                    </li>
+                    <li>
+                        <h2>9- Protection en Cas de Piratage
+                        </h2>
+                        <p>• Mesures Proactives : Nous utilisons des mesures de sécurité avancées, telles que la surveillance des réseaux, les tests de pénétration réguliers et les protocoles de détection d’intrusion, pour prévenir les attaques de piratage.<br>
+                        • Réponse aux Incidents : En cas de piratage, nous avons des plans de réponse aux incidents pour minimiser l’impact, contenir l’incident et remédier à toute vulnérabilité. Cela inclut l’isolement des systèmes affectés, la notification rapide des autorités compétentes et des utilisateurs concernés, et la mise en œuvre de correctifs.<br>
+                        • Assurance et Responsabilité : Bien que nous prenions toutes les mesures possibles pour protéger vos données, [Nom de l'Organisation] ne peut être tenu responsable des conséquences d’un piratage informatique. Nous vous recommandons de prendre des précautions personnelles pour protéger vos informations.</p>
+                    </li>
+                    <li>
+                        <h2>10- Contact
+                        </h2>
+                        <p>• Pour toute question ou préoccupation concernant cette politique de confidentialité ou pour exercer vos droits, vous pouvez nous contacter à l’adresse suivante : [Adresse e-mail de contact].</p>
+                    </li>
+                    <li>
+                        <P>                        Cette politique de confidentialité est conçue pour protéger [Nom de l'Organisation] en définissant clairement les types de données collectées, leur utilisation, les mesures de sécurité, les droits des utilisateurs, l’utilisation des cookies, la conservation des données et les conditions de partage des données. Elle garantit également la transparence et la conformité légale des pratiques de traitement des données de [Nom de l'Organisation] et inclut des mesures spécifiques en cas de piratage pour protéger les utilisateurs et l’entreprise.
+                        </P>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="footer_pages.js"></script>
+</html>
